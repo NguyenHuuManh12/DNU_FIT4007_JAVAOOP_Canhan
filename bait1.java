bef8d4e4041294fa4b9139cf88f2065712e9daf5
NguyenHuuManh12 <manhnh3112@gmail.com>
2025-09-11 23:25:44 +0700

    Add files via upload

---
+++ bait1.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class bait1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // 2 số nguyên
+        System.out.print("Nhập a: ");
+        int a = sc.nextInt();
+        System.out.print("Nhập b: ");
+        int b = sc.nextInt();
+        // tính
+        int tong = a + b;
+        int tich = a - b;
+        int hieu = a * b;
+        // lỗi chia cho 0
+        if (b != 0 ){
+            float thuong = (float) a/b;
+            System.out.print("Thường: " + thuong);
+        } else {
+            System.out.print("Không chia cho 0!");
+        }
+        // in kq
+        System.out.print("Tong: " + tong);
+        System.out.print("Tích: " + tich);
+        System.out.print("Hieu: " + hieu);
+        sc.close();
+    }
+}

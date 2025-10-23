515cbaec8c26a1053f099e0e43f60224b4906b29
Nguyễn Hữu Mạnh <manhnh3112@gmail.com>
2025-10-23 08:15:38 +0700

    update Part2 Ex12CheckCapitalChar

---
+++ src/Part2ConditionalStatements/Ex12CheckCapitalChar.java
@@ -0,0 +1,19 @@
+package Part2ConditionalStatements;
+
+import java.util.Scanner;
+
+public class Ex12CheckCapitalChar {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Nhập một ký tự: ");
+        char ch = scanner.next().charAt(0);
+
+        if (ch >= 'A' && ch <= 'Z') {
+            System.out.println(ch + " là chữ cái in hoa.");
+        } else {
+            System.out.println(ch + " không phải là chữ cái in hoa.");
+        }
+    }
+
+}

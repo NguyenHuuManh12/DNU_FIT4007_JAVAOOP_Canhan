bbbaa7494ec7a0e2e6328917d147fc97f178c3c3
Nguyễn Hữu Mạnh <manhnh3112@gmail.com>
2025-10-24 09:47:28 +0700

    update Part4 Ex5EvenNumbersUpToN

---
+++ src/Part4Loops/Ex5EvenNumbersUpToN.java
@@ -0,0 +1,22 @@
+package Part4Loops;
+
+import java.util.Scanner;
+
+public class Ex5EvenNumbersUpToN {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Nhập số nguyên dương N: ");
+        int N = sc.nextInt();
+
+        if (N <= 0) {
+            System.out.println("Vui lòng nhập một số nguyên dương.");
+        } else {
+            System.out.println("Các số chẵn từ 1 đến " + N + " là:");
+            for (int i = 2; i <= N; i += 2) {
+                System.out.println(i);
+            }
+        }
+    }
+
+}

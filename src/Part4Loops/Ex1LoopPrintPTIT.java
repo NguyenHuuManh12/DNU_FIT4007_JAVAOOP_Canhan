bd05aa54c40eba808fc40d3ed520a0b95aa9e6b2
Nguyễn Hữu Mạnh <manhnh3112@gmail.com>
2025-10-24 08:36:27 +0700

    update Part4Loops Ex1LoopPrintPTIT

---
+++ src/Part4Loops/Ex1LoopPrintPTIT.java
@@ -0,0 +1,9 @@
+package Part4Loops;
+
+public class Ex1LoopPrintPTIT {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 20; i++) {
+            System.out.println("PTIT");
+        }
+    }
+}
